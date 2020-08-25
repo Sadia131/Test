647b18f6dad3c59742e574a6081e68ecdb55b01f
sadia <sadiaobaed@gmail.com>
2020-08-25 13:42:43 -0700

    commit

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author User
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x=9;
+        int y=10;
+        System.out.println(x+y);
+    }
+    
+}
